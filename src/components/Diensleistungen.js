43e1e3c7709019e4031baf79666414ac7728838b
aytlgokdemir <117666528+aytlgokdemir@users.noreply.github.com>
2023-12-29 17:19:45 +0100

    services

--- src/components/Diensleistungen.js
+++ src/components/Diensleistungen.js
@@ -33,11 +33,11 @@ const callouts = [
 
 export default function Example() {
   return (
-    <div id="diensleistungen" className="bg-gray-100">
+    <div id="dienstleistungen" className="bg-gray-100">
       <div className="mx-auto max-w-6xl px-4 sm:px-8">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
           <h2 className="text-xl sm:text-2xl lg:text-3xl lg:-ml-12 font-bold text-gray-900 ">
-            DIENSLEISTUNGEN
+            DIENSTLEISTUNGEN
           </h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-2 lg:gap-x-12 lg:gap-y-10 lg:space-y-0 ">
